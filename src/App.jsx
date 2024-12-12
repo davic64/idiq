1be732181bde89d741064562119d57eb2ea192cc
David Victoria <davidvictoria64@gmail.com>
2024-12-12 02:15:25 -0600

    feat: ✨ Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { routes } from "./routes";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<route.component />}
+          />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
